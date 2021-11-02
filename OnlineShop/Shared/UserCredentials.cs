c852b8fe8bec95fc75b240a949bbefb26833c648
fivan <46835435+f1van0@users.noreply.github.com>
2021-11-02 15:23:41 +0700

    added login and register pages

---
+++ OnlineShop/Shared/UserCredentials.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Shared
+{
+    public record UserCredentials
+    {
+        public string Login;
+
+        public string Password;
+    }
+}
