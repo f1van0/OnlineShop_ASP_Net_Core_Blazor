544a0d26b709d8895eebd563856aa10ce1a1d699
Optifun <frdima19@gmail.com>
2021-11-06 23:33:31 +0700

    Update AuthController: add docs + correct statuses

--- OnlineShop/Server/Controllers/AuthController.cs
+++ OnlineShop/Server/Controllers/AuthController.cs
@@ -14,7 +14,7 @@ namespace OnlineShop.Server.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
-        private IUserRepository _userRepository;
+        private readonly IUserRepository _userRepository;
 
         //Первичная инициализация _userRepository для дальнейшей работы
         public AuthController(IUserRepository userRepository)
@@ -22,25 +22,50 @@ namespace OnlineShop.Server.Controllers
             _userRepository = userRepository;
         }
 
-        //С помощью POST из Body по пришедшим состороны пользователя credentials, система пытается зарегистрировать его в системе и возвращает пользователю результат
+        /// <summary>
+        /// Registers user with credentials
+        /// </summary>
+        /// <param name="credentials"></param>
+        /// <returns>User's data with cookie</returns>
+        /// <responce code="200">Returns user data</responce>
+        /// <responce code="400">Returns if user credentials are empty or already in use</responce>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
+        //С помощью POST из Body по пришедшим состороны пользователя credentials, система пытается
+        // зарегистрировать его в системе и возвращает пользователю результат
         public ActionResult<User> Post([FromBody] UserCredentials credentials)
         {
             var user = _userRepository.Register(credentials);
+            if (user == null)
+                return BadRequest();
+
             return Ok(user);
         }
 
-        //С помощью PUT из Body по пришедшим состороны пользователя credentials, система пытается залогинить (найти сведения, тождественные credentials) и возвращает пользователю результат
         /// <summary>
         /// Login user from credentials
         /// </summary>
         /// <param name="credentials"></param>
-        /// <returns></returns>
+        /// <returns>User's data with cookie</returns>
+        /// <responce code="200">Returns user data</responce>
+        /// <responce code="401">Returns if user credentials don't exist</responce>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [Produces("application/json")]
+        //С помощью PUT из Body по пришедшим состороны пользователя credentials, система пытается
+        // залогинить (найти сведения, тождественные credentials) и возвращает пользователю результат
         public ActionResult<User> Put([FromBody] UserCredentials credentials)
         {
             var user = _userRepository.Login(credentials);
+            if (user == null)
+                return Unauthorized();
+
+            HttpContext.Response.Cookies.Append("auth", $"{user.Login}, {user.Password}");
             return Ok(user);
+
         }
     }
 }
