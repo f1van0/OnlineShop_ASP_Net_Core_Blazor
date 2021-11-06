41e77ccbe5f527b5c8e4b4f4bd00091bcf75323a
Optifun <frdima19@gmail.com>
2021-11-06 23:56:13 +0700

    Update UserCredentials: add data annotations

--- OnlineShop/Shared/UserCredentials.cs
+++ OnlineShop/Shared/UserCredentials.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace OnlineShop.Shared
 {
     public record UserCredentials
     {
+        [Required]
+        [StringLength(maximumLength: 12, MinimumLength = 4)]
         public string Login { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 16, MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
