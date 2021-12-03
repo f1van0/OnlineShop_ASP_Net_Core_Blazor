0bfb1730065ae61b165a5b3c3f8adf2ac0553192
fivan <46835435+f1van0@users.noreply.github.com>
2021-12-04 01:54:19 +0700

    mudblazor was implemented and bootstrap was removed

--- OnlineShop/Client/Program.cs
+++ OnlineShop/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -13,7 +14,8 @@ namespace OnlineShop.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
