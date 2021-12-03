574096809ec9f9a29b5f4c05d12c79f758efb330
Optifun <frdima19@gmail.com>
2021-12-03 22:05:01 +0700

    Update DataAccess: extend DataAccess

--- OnlineShop/Server/DB/DataAccess.cs
+++ OnlineShop/Server/DB/DataAccess.cs
@@ -16,14 +16,55 @@ namespace OnlineShop.Server.DB
             this._connection = connection;
         }
 
+        public async Task<List<T>> Select<T>(string sql)
+        {
+            var rows = await _connection.QueryAsync<T>(sql);
+            return rows.ToList();
+        }
+        
         public async Task<List<T>> Select<T, U>(string sql, U parameters)
         {
             var rows = await _connection.QueryAsync<T>(sql, parameters);
             return rows.ToList();
         }
 
-        public async Task<int> Query<T>(string sql, T parameters) => 
-            await _connection.ExecuteAsync(sql, parameters);
+        public async Task<List<T>> Select<T, S, U>(string sql, Func<T, S, T> mapper, U parameters)
+        {
+            var rows = await _connection.QueryAsync(sql, mapper, parameters);
+            return rows.ToList();
+        }
 
+        public async Task<List<T>> Select<T, S, S1, U>(string sql, Func<T, S, S1, T> mapper, U parameters)
+        {
+            var rows = await _connection.QueryAsync(sql, mapper, parameters);
+            return rows.ToList();
+        }
+
+        public async Task<List<T>> Select<T, S, S1, S2, U>(string sql, Func<T, S, S1, S2, T> mapper, U parameters)
+        {
+            var rows = await _connection.QueryAsync(sql, mapper, parameters);
+            return rows.ToList();
+        }
+
+        public async Task<List<T>> Select<T, S, S1, S2, S3, U>(string sql, Func<T, S, S1, S2, S3, T> mapper, U parameters)
+        {
+            var rows = await _connection.QueryAsync(sql, mapper, parameters);
+            return rows.ToList();
+        }
+
+        public async Task<List<T>> Select<T, S, S1, S2, S3, S4, U>(string sql, Func<T, S, S1, S2, S3, S4, T> mapper, U parameters)
+        {
+            var rows = await _connection.QueryAsync(sql, mapper, parameters);
+            return rows.ToList();
+        }
+
+        public async Task<List<T>> Select<T, S, S1, S2, S3, S4, S5, U>(string sql, Func<T, S, S1, S2, S3, S4, S5, T> mapper, U parameters)
+        {
+            var rows = await _connection.QueryAsync(sql, mapper, parameters);
+            return rows.ToList();
+        }
+        
+        public async Task<int> Query<T>(string sql, T parameters) =>
+            await _connection.ExecuteAsync(sql, parameters);
     }
 }
