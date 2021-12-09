f68843349a597ac102e5c18f5d9098480a9e8eab
fivan <46835435+f1van0@users.noreply.github.com>
2021-12-09 21:03:57 +0700

    + paramsDB class (now palettes and imageSizes available from this class) + fixes in imageDB

--- OnlineShop/Server/DB/ImagesDB.cs
+++ OnlineShop/Server/DB/ImagesDB.cs
@@ -14,29 +14,38 @@ namespace OnlineShop.Server.DB
             _db = db;
         }
 
-        public Task<List<UserMosaic>> UserMosaic(int userId)
+        public Task<List<UserImage>> GetUserImages(int userId)
         {
             var sql = "SELECT * FROM online_shop.images WHERE OwnerID=@userId;";
-            return _db.Select<UserMosaic, ColourPalette, dynamic>(sql, (m, p) =>
+            return _db.Select<UserImage, int, dynamic>(sql, (m, p) =>
             {
-                m.Palette = p;
+                m.PaletteID = p;
                 return m;
             }, new {userId});
         }
 
-        public Task SaveMosaic(User user, UserMosaic mosaic)
+        public Task SaveUserImage(int userID, UserImage image)
         {
             var sql = @"INSERT INTO online_shop.images 
-            (OwnerID, Name, Size, Image, ColorPalette, Date)
+            (OwnerID, Name, SizeID, Image, ColorPaletteID, Date)
             VALUES (@OwnerID, @Name, @Size, @Image, @ColorPaletteID, @Date)";
             return _db.Query(sql, new
             {
-                OwnerID = user.ID,
-                Name = mosaic.Name,
-                Size = mosaic.Size,
-                Image = mosaic.Image,
-                ColorPaletteID = mosaic.Palette.ID,
-                Date = mosaic.Date
+                OwnerID = userID,
+                Size = image.SizeID,
+                Image = image.Image,
+                ColorPaletteID = image.PaletteID,
+                Date = image.Date
+            });
+        }
+
+        public Task SetColorPaletteOfUserImage(int userID, UserImage userImage)
+        {
+            var sql = @"UPDATE online_shop.images SET colorPaletteID=@ColorPaletteID WHERE ID=@ID";
+            return _db.Query<dynamic>(sql, new
+            {
+                ColorPaletteID = userImage.PaletteID,
+                ID = userID
             });
         }
     }
---
+++ OnlineShop/Server/DB/ParamsDB.cs
@@ -0,0 +1,53 @@
+﻿using OnlineShop.Server.DB.Mappers;
+using OnlineShop.Shared;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Server.DB
+{
+    public class ParamsDB
+    {
+            public IDataAccess _db;
+
+            public ParamsDB(IDataAccess db)
+            {
+                _db = db;
+            }
+
+            public async Task<ColorPalette[]> GetPalettes()
+            {
+                string sql = "SELECT * FROM online_shop.color_palettes";
+                var colorPalettes = await _db.Select<ColorPalette>(sql);
+                return colorPalettes.ToArray();
+            }
+
+            public async Task<ImageSize[]> GetImageSizes()
+            {
+                string sql = "SELECT * FROM online_shop.image_sizes";
+                var imageSizes = await _db.Select<ImageSize>(sql);
+                return imageSizes.ToArray();
+            }
+
+            public async Task<bool> PaletteExists(int paletteID)
+            {
+                string sql = "SELECT * FROM online_shop.color_palettes WHERE ID = @ID;";
+                var colorPalette = await _db.Select<ColorPalette, dynamic>(sql, new {ID = paletteID});
+
+                if (colorPalette.FirstOrDefault() != null)
+                    return true;
+                else
+                    return false;
+            }
+            
+            public async Task<bool> ImageSizeExists(int sizeID)
+            {
+                string sql = "SELECT * FROM online_shop.image_sizes WHERE ID = @ID;";
+                var imageSize = await _db.Select<ColorPalette, dynamic>(sql, new {ID = sizeID});
+
+                if (imageSize.FirstOrDefault() != null)
+                    return true;
+                else
+                    return false;
+            }
+    }
+}
