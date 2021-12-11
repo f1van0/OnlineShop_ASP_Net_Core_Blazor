03b1eed246372d96de56e5a9bec4d827ed4f9fbe
Optifun <frdima19@gmail.com>
2021-12-11 19:25:39 +0700

    Add Navigation service

--- OnlineShop/Client/Program.cs
+++ OnlineShop/Client/Program.cs
@@ -19,6 +19,7 @@ namespace OnlineShop.Client
 
             builder.Services.AddSingleton(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
             builder.Services.AddSingleton<AppState>();
+            builder.Services.AddSingleton<Navigation>();
             builder.Services.AddTransient<CookieStorage>();
 
             builder.Services.AddMudServices();
