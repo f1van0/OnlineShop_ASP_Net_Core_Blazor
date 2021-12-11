ab291ee878e8186ba3297c5e6a5b1d7ef6ee44d7
Optifun <frdima19@gmail.com>
2021-12-11 16:12:50 +0700

    Update ImageController: Upload image prototype

--- OnlineShop/Server/Controllers/ImageController.cs
+++ OnlineShop/Server/Controllers/ImageController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using OnlineShop.Shared;
 using OnlineShop.Server.DB;
 using OnlineShop.Server.Services;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
@@ -15,71 +16,79 @@ namespace OnlineShop.Server.Controllers
     [Route("api/[controller]")]
     public class ImageController : ControllerBase
     {
-                private readonly ImagesDB _imagesDb;
-                private ILogger<ImageController> _logger;
-        
-                //Первичная инициализация _catalogManager для дальнейшей работы
-                public ImageController(ImagesDB imagesDB, ILogger<ImageController> logger)
-                {
-                    _logger = logger;
-                    _imagesDb = imagesDB;
-                }
-                
-                [Authorize]
-                [HttpGet("[action]")]
-                //С помощью GET пользователю возвращается список его картинок
-                public async Task<IEnumerable<UserImage>> GetImages()
-                {
-                    JwtSecurityToken jwtSecurityToken = HttpContext.Request.GetToken();
-                    var payload = jwtSecurityToken.GetPayload<JWTPayload>();
-                    if (payload.UserId != -1)
-                    {
-                        var userImages = await _imagesDb.GetUserImages(payload.UserId);
-                        return userImages;
-                    }
-                    else
-                    {
-                        return null;
-                    }
-                }
-                
-                [Authorize]
-                [HttpPost("[action]")]
-                //С помощью POST из Body по пришедшей со стороны пользователя картинке, система пытается
-                //добавить её в список картинок
-                public ActionResult<UserImage> UploadImage([FromBody] UserImage userImage)
-                {
-                    JwtSecurityToken jwtSecurityToken = HttpContext.Request.GetToken();
-                    var payload = jwtSecurityToken.GetPayload<JWTPayload>();
-                    if (payload.UserId != -1)
-                    {
-                        // if (_catalogDb.BuyGoods(payload.UserId, goodsID).IsCompleted)
-                        // {
-                        //     return Ok(ResponseStatus.Completed);
-                        // }
-                        // else
-                        // {
-                        //     return BadRequest(ResponseStatus.Failed);
-                        // }
-                    }
+        private readonly ImagesDB _imagesDb;
+        private ILogger<ImageController> _logger;
 
-                    return Unauthorized();
-                }
-                
-                [Authorize]
-                [HttpPost("[action]")]
-                //С помощью POST из Body по пришедшей со стороны пользователя картинке с измененным цветом, система пытается
-                //изменить цвет у заданной картинки
-                public ActionResult<UserImage> SetImageColor([FromBody] UserImage userImage)
-                {
-                    JwtSecurityToken jwtSecurityToken = HttpContext.Request.GetToken();
-                    var payload = jwtSecurityToken.GetPayload<JWTPayload>();
-                    if (payload.UserId != -1)
-                    {
-                        _imagesDb.SetColorPaletteOfUserImage(payload.UserId, userImage);
-                    }
+        //Первичная инициализация _catalogManager для дальнейшей работы
+        public ImageController(ImagesDB imagesDB, ILogger<ImageController> logger)
+        {
+            _logger = logger;
+            _imagesDb = imagesDB;
+        }
 
-                    return Unauthorized();
+        [Authorize]
+        [HttpGet("[action]")]
+        //С помощью GET пользователю возвращается список его картинок
+        public async Task<IEnumerable<UserImage>> GetImages()
+        {
+            JwtSecurityToken jwtSecurityToken = HttpContext.Request.GetToken();
+            var payload = jwtSecurityToken.GetPayload<JWTPayload>();
+            if (payload.UserId != -1)
+            {
+                var userImages = await _imagesDb.GetUserImages(payload.UserId);
+                return userImages;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [Authorize]
+        [HttpPost("[action]")]
+        //С помощью POST из Body по пришедшей со стороны пользователя картинке, система пытается
+        //добавить её в список картинок
+        public async Task<ActionResult<UserImage>> UploadImage([FromBody] SaveImageRequest image)
+        {
+            JwtSecurityToken jwtSecurityToken = HttpContext.Request.GetToken();
+            var payload = jwtSecurityToken.GetPayload<JWTPayload>();
+            int userId = payload.UserId;
+            // if (image.Size.SizeX != image.Pixels[0].Length || image.Size.SizeY != image.Pixels.Length)
+            //     return BadRequest();
+
+            if (userId != -1)
+            {
+                try
+                {
+                    UserImage userImage = await _imagesDb.SaveUserImage(userId, image.Pixels, paletteId: image.Palette.ID, sizeId: image.Size.ID);
+                    if (userImage != null)
+                        return Ok(userImage);
                 }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Exception while inserting image to DB");
+                }
+
+                return StatusCode(500);
+            }
+
+            return Unauthorized();
+        }
+
+        [Authorize]
+        [HttpPost("[action]")]
+        //С помощью POST из Body по пришедшей со стороны пользователя картинке с измененным цветом, система пытается
+        //изменить цвет у заданной картинки
+        public ActionResult<UserImage> SetImageColor([FromBody] UserImage userImage)
+        {
+            JwtSecurityToken jwtSecurityToken = HttpContext.Request.GetToken();
+            var payload = jwtSecurityToken.GetPayload<JWTPayload>();
+            if (payload.UserId != -1)
+            {
+                _imagesDb.SetColorPaletteOfUserImage(payload.UserId, userImage);
+            }
+
+            return Unauthorized();
+        }
     }
 }
